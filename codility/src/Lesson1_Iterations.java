46aab1b938a01bcc4adffd5e491f0e66927dc6a9
sungtae1.kang <sungtae1.kang>
2020-06-15 11:34:40 +0900

    codility lesson_1

---
+++ codility/src/Lesson1_Iterations.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson1_Iterations {
+
+    @Test
+    public void test() {
+        System.out.println(solution(112312300));
+    }
+
+    public int solution(int N) {
+        String binaryStr = Integer.toBinaryString(N);
+        System.out.println(binaryStr);
+        int length = binaryStr.length();
+        int maxZeroCnt = 0;
+        int zeroCnt = 0;
+
+        for (int i = 0; i < length; i++) {
+            if ('0' == binaryStr.charAt(i)) {
+                zeroCnt++;
+            } else {
+                maxZeroCnt = Math.max(zeroCnt, maxZeroCnt);
+                zeroCnt = 0;
+            }
+        }
+        return maxZeroCnt;
+    }
+}
